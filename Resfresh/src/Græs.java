08c77bc57bf297610007f75480aa033a52073cb2
JOBMalmstroem <jolaf3003@gmail.com>
2021-04-06 15:50:01 +0200

    1. added græs 2. added scannere

--- Resfresh/src/Græs.java
+++ Resfresh/src/Græs.java
@@ -3,12 +3,13 @@ public class Græs {
     public static void main(String[] args) {
         System.out.println("hvor langt er dit græs i cm?");
         Scanner grassLength = new Scanner(System.in);
-        int gL = grassLength.nextInt();
+        double gL = grassLength.nextDouble();
 
 
         System.out.println("Hvilken længde skal det have før det skal slås?");
         Scanner cutWhen = new Scanner(System.in);
-        int cW = cutWhen.nextInt();
+        double cW = cutWhen.nextDouble();
 
+        for (double i=gL; i<=cW; i+=0.8);
     }
 }
