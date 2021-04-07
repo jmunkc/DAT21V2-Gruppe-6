97b6fddedb5de4c685f56478cd716993fd645402
Lars <larsbrogaard86@gmail.com>
2021-04-07 09:23:19 +0200

    Færdig

--- Resfresh/src/Kvadrat.java
+++ Resfresh/src/Kvadrat.java
@@ -1,2 +1,16 @@
 public class Kvadrat {
+
+    public static void printSquare(int numberInput, String symbolInput){
+
+        int sides = numberInput;
+        String symbolPrint = symbolInput;
+
+        for (int i = 0; i < sides; i++){
+            for(int j = 0; j < sides; j++){
+                System.out.print(symbolPrint);
+            }
+            System.out.println("");
+        }
+    }
+
 }
