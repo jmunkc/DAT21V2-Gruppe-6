7286e37bff04de2d152a2cbfe6f802b395a6075d
KevinFHansen <kevi4777@stud.kea.dk>
2021-04-08 11:15:20 +0200

    Final Sortering

--- Resfresh/src/Sortering.java
+++ Resfresh/src/Sortering.java
@@ -17,26 +17,6 @@ public class Sortering {
         System.out.println(problemDescription);
         }
 
-    public static void sorterProblemet(){
-        Scanner sorteringsScanner = new Scanner(System.in);
-        System.out.println("Indtast 5 ord");
-        String firstWord = sorteringsScanner.nextLine();
-        String secondWord = sorteringsScanner.nextLine();
-        String thirdWord = sorteringsScanner.nextLine();
-        String fourthWord = sorteringsScanner.nextLine();
-        String fifthWord = sorteringsScanner.nextLine();
-
-        sorteringArrayList.add(firstWord);
-        sorteringArrayList.add(secondWord);
-        sorteringArrayList.add(thirdWord);
-        sorteringArrayList.add(fourthWord);
-        sorteringArrayList.add(fifthWord);
-        Collections.sort(sorteringArrayList);
-        System.out.println(sorteringArrayList);
-        Collections.reverse(sorteringArrayList);
-        System.out.println(sorteringArrayList);
-        }
-
     public static void sorterProblem1(){
         Scanner sorteringsScanner = new Scanner(System.in);
         System.out.println("Type 5 Strings");
